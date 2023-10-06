da3f0c583373a34e70eb8152e53e55867330b60d
vinaygit <vinaykss98@gmail.com>
2023-10-06 12:14:36 +0530

    myntra suit deve by jr dev

---
+++ src/main/java/Suite2020/output/App.java
@@ -0,0 +1,13 @@
+package Suite2020.output;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Suite2020/output/AppTest.java
@@ -0,0 +1,20 @@
+package Suite2020.output;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
